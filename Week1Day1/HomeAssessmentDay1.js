b764f0c0fc7db758f5cf1f48ec11302d8b458ef9
nandhiniswisdom-np <nandhiniswisdom@gmail.com>
2026-03-17 15:43:31 +0530

    Add files via upload
    Week1Day1 Home-Assignment

---
+++ Week1Day1/HomeAssessmentDay1.js
@@ -0,0 +1,24 @@
+//string
+let userName="Nandhini";
+console.log(userName);
+console.log(typeof(userName));
+
+//string
+let organization="Testleaf";
+console.log(organization);
+console.log(typeof(organization));
+
+//number
+let contactNumber=9988776655;
+console.log(contactNumber);
+console.log(typeof(contactNumber));
+
+//boolean
+let knowsAutomation=true;
+console.log(knowsAutomation);
+console.log(typeof(knowsAutomation));
+
+//undefined
+let userPlaywright;
+console.log(userPlaywright);
+console.log(typeof(userPlaywright));
---
+++ Week1Day1/datatype.js
@@ -0,0 +1,31 @@
+//string
+var EmployeeName="Hari";
+console.log(EmployeeName);
+console.log(typeof(EmployeeName));
+
+//number
+var mobilenumber=1234567890;    
+console.log(mobilenumber);      
+console.log(typeof(mobilenumber));
+
+//boolean
+var isPlaywright=true;
+console.log(isPlaywright);
+console.log(typeof(isPlaywright));
+
+//undefined =if there is no value assigned to the variable it will be undefined, 
+// lateron we can assign value to it
+var empid;
+console.log(empid);
+console.log(typeof(empid));
+
+//null
+var empid=null;
+console.log(empid);
+console.log(typeof(empid));
+
+//big int = by specifying n at the end of the number we can declare big int,
+//  it is used to store large numbers
+var accountNumber=1234567890123456789012345678901234567890n;
+console.log(accountNumber);
+console.log(typeof(accountNumber));
